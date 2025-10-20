43c0131e2dbb780028ea3d0ee3fa0a236cd609fc
ImStillSalmon <109127603+ImStillSalmon@users.noreply.github.com>
2025-10-20 00:45:07 -0700

    Add files via upload

---
+++ cj1-1.py
@@ -0,0 +1,8 @@
+def main():
+    print("Full Name: Eli Weiss")
+    print("Pronouns: he/him")
+    print("My favorite movie is monty python and the holy grail")
+    print("My favorite food is probably just a really good burger)
+
+if __name__ == "__main__":
+    main()
